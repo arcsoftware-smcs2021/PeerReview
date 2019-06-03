2dcf2a7123ecbcbd476e6096829a26e63ee6cef9
rbhog <rbhogavilli@gmail.com>
2019-06-03 18:26:57 -0400

    Add anonymization functionality

--- src/anonymize.js
+++ src/anonymize.js
@@ -1,17 +1,30 @@
-function anonymize(document,name){
-  var fs = require("fs");
-  var docx = require("docx");
+var good_stuff = require('./doc.js');
 
-  fs.readFile(document, function (err, data) {
-    if(data.includes('Name')){
-      data = data.toString();
-      var doc = data.replace('Name','XXXXX')
-      fs.writeFileSync(document, doc);
+function anonymize(document_path) {
+  good_stuff.edit(document).then(function(res, err) {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log()
     }
-
-  });
-
+  })
 }
 
-//anonymize("C:/Users/ronoy/anonymization/test.docx","name"); // sample call
-module.exports.anonymize = anonymize;
+//
+// function anonymize(document,name){
+//   var fs = require("fs");
+//   var docx = require("docx");
+//
+//   fs.readFile(document, function (err, data) {
+//     if(data.includes('Name')){
+//       data = data.toString();
+//       var doc = data.replace('Name','XXXXX')
+//       fs.writeFileSync(document, doc);
+//     }
+//
+//   });
+//
+// }
+//
+// //anonymize("C:/Users/ronoy/anonymization/test.docx","name"); // sample call
+// module.exports.anonymize = anonymize;
