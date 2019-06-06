239d57174e4ddb131aa5c6bf33d87acc72e5415e
Aidan Goettsch <yayes.aidan@gmail.com>
2019-06-05 22:12:14 -0400

    trust proxy

--- src/app.js
+++ src/app.js
@@ -78,6 +78,9 @@ app.set('port', port)
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+// Trust the Heroku proxy
+app.set('trust proxy', true)
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
